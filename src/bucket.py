622beaac9df4418b05468a9e93ace20fa0ecc913
Vernon Swanepoel <frantic.naturalist@gmail.com>
2022-10-09 20:14:32 +0100

    Add method added to Bucket class

--- src/bucket.py
+++ src/bucket.py
@@ -1,3 +1,9 @@
-"""Collect New Ideas that could be projects or things to do."""
+"""Collect new ideas that could be projects or things to do."""
 
-pass
+
+class Bucket:
+    def __init__(self, bucket_dir='.bucket', bucket_file='bucket.csv', delimiter='|', logging=True):
+        self.bucket = []
+
+    def add(self, item):
+        self.bucket.append(item)
--- tests/test_bucket.py
+++ tests/test_bucket.py
@@ -1,3 +1,10 @@
 """Test for the bucket module."""
 
-pass
+from src.bucket import Bucket
+
+
+class TestBucket(object):
+    def test_add(self):
+        bucket = Bucket()
+        bucket.add('item')
+        assert bucket.bucket == ['item']
