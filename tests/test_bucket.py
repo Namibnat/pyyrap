6e896275296841196af0b64a2315a1a95de6564f
Vernon Swanepoel <frantic.naturalist@gmail.com>
2022-10-10 20:28:50 +0100

    Add date check to tests for new bucket items

--- tests/test_bucket.py
+++ tests/test_bucket.py
@@ -1,5 +1,7 @@
 """Test for the bucket module."""
 
+import datetime
+
 from src.bucket import Bucket
 
 
@@ -8,6 +10,12 @@ class TestBucket(object):
         bucket = Bucket()
         bucket.add('item')
         assert bucket.bucket[0].item == 'item'
+
+    def test_date_created(self):
+        bucket = Bucket()
+        bucket.add('item')
+        assert bucket.bucket[0].date_created is not None
+        assert bucket.bucket[0].date_created.year == datetime.datetime.now().year
 
     def test_remove(self):
         bucket = Bucket()
