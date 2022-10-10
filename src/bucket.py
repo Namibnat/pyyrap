36d83aa7da45299b89dad58c5194c71753cd66cc
Vernon Swanepoel <frantic.naturalist@gmail.com>
2022-10-10 20:10:33 +0100

    Fleshing out the 'Bucket' class more

--- src/bucket.py
+++ src/bucket.py
@@ -1,9 +1,23 @@
 """Collect new ideas that could be projects or things to do."""
 
+import os
+
 
 class Bucket:
-    def __init__(self, bucket_dir='.bucket', bucket_file='bucket.csv', delimiter='|', logging=True):
+    def __init__(self, bucket_dir='.yrap', bucket_file='bucket.csv', delimiter='|', logging=True):
         self.bucket = []
+        self.bucket_dir = bucket_dir
+        self.bucket_file = bucket_file
+        self.delimiter = delimiter
+        self.logging = logging
+        self.home_directory = os.path.expanduser('~')
+        self.bucket_path = os.path.join(self.home_directory, self.bucket_dir, self.bucket_file)
+
+    def create_bucket_file(self):
+        if not os.path.exists(self.bucket_path):
+            os.makedirs(os.path.dirname(self.bucket_path), exist_ok=True)
+            with open(self.bucket_path, 'w') as f:
+                f.write('')
 
     def add(self, item):
         self.bucket.append(item)
